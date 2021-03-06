cb83f9d202c1e654cfa9bb028d918304e23875f5
ShValerii <77387781+ShValerii@users.noreply.github.com>
2021-03-06 17:25:21 +0200

    Add files via upload

--- logic.py
+++ logic.py
@@ -1,131 +1,127 @@
 import text
 
-def inputnumb() :
+
+def inputnumb():
     numb = 0
-    try :
-        numb = int(input("Для перехода, введите номер задачи:"))
-        if 1<= int(numb) <=5 :
-            choisetask(numb)
-        else :
-            text.repeat()
-            inputnumb()
-    except ValueError :
-        text.repeat()
-        inputnumb()
+    numb = input("Для перехода, введите номер задачи:")
+    choisetask(numb)
 
 
-def choisetask(numb) :
-    if numb == 1 :
+def choisetask(numb):
+    if numb=='1':
         text.tasktext1()
         text.taskmenuprint()
         taskmenu(numb)
-    elif numb == 2 :
+    elif numb=='2':
         text.tasktext2()
         text.taskmenuprint()
         taskmenu(numb)
-    elif numb == 3 :
+    elif numb=='3':
         text.tasktext3()
         text.taskmenuprint()
         taskmenu(numb)
-    elif numb == 4 :
+    elif numb=='4':
         text.tasktext4()
         text.taskmenuprint()
         taskmenu(numb)
-    elif numb == 5 :
+    elif numb=='5':
         text.tasktext5()
         text.taskmenuprint()
         taskmenu(numb)
-    else :
+    else:
         text.taskexeption()
         inputnumb()
 
-def taskmenu(numb) :
+
+def taskmenu(numb):
     x = str(input('Поле ввода:'))
     x = x.lower()
-    if x == 'решить' or x == 'код' or x == 'ответ' :
+    if x=='решить' or x=='код' or x=='ответ':
         task(x, numb)
-    else :
+    else:
         text.repeat()
         taskmenu(numb)
 
-def taskcode(numb) :
-    if numb == 1 :
+
+def taskcode(numb):
+    if numb==1:
         j, answ = 0, 0
-        for i in range(1, 7) :
+        for i in range(1, 7):
             j = 9 - i
-            if (2 * i) + (3 * j) == 23 :
+            if (2 * i) + (3 * j)==23:
                 answ = j
         return answ
-    elif numb == 2 :
+    elif numb==2:
         j = 0
-        for i in range(1, 11) :
+        for i in range(1, 11):
             j = 19 - i
-            if i + 2 * j == 28 :
+            if i + 2 * j==28:
                 return j
-    elif numb == 3 :
+    elif numb==3:
         j = 0
-        for i in range(1, int(68 / 6) + 1) :
+        for i in range(1, int(68 / 6) + 1):
             j = 10 - i
-            if 6 * i + 8 * j == 68 :
+            if 6 * i + 8 * j==68:
                 return j
-    elif numb == 4 :
+    elif numb==4:
         b, c = 0, 0
-        for a in range(1, 16) :
+        for a in range(1, 16):
             b = 1
             c = a - b
-            if a + b + c == 16 and a - b == c :
+            if a + b + c==16 and a - b==c:
                 return a
-    elif numb == 5 :
-        for kira in range(1, int(81 / 4)) :
-            if kira + kira * 5 + kira * 5 * 5 + kira * 5 * 5 * 2 == 81 :
+    elif numb==5:
+        for kira in range(1, int(81 / 4)):
+            if kira + kira * 5 + kira * 5 * 5 + kira * 5 * 5 * 2==81:
                 return kira * 5
 
-def code(numb) :
-    if numb==1 :
+
+def code(numb):
+    if numb==1:
         text.code1()
-    elif numb==2 :
+    elif numb==2:
         text.code2()
-    elif numb==3 :
+    elif numb==3:
         text.code3()
-    elif numb==4 :
+    elif numb==4:
         text.code4()
-    elif numb==5 :
+    elif numb==5:
         text.code5()
     text.endtaskprint()
     endtask(numb)
 
-def task(x, numb) :
-    if x == 'решить' :
-        while taskcode(numb) != text.yourtry() :
+
+def task(x, numb):
+    if x=='решить':
+        while taskcode(numb)!=text.yourtry():
             print('Не правильно')
-        else :
+        else:
             print('Правильно')
             text.endtaskprint()
             endtask(numb)
-
-    elif x == 'код' :
+    elif x=='код':
         code(numb)
-
-    elif x == 'ответ' :
+    elif x=='ответ':
         print('Ответ:', taskcode(numb))
         text.endtaskprint()
         endtask(numb)
-
-    else :
+    else:
         text.repeat()
         task(x, numb)
 
-def endtask(numb) :
+
+def endtask(numb):
     y = str(input('Поле ввода:'))
     y = y.lower()
-    if y=='назад' or y=='меню' or y=='выход' :
-        if y == 'назад' :
+    if y=='назад' or y=='меню' or y=='выход':
+        if y=='назад':
             text.taskmenuprint()
             taskmenu(numb)
-        elif y == 'меню' :
+        elif y=='меню':
             text.taskslist()
             inputnumb()
-        elif y == 'выход' :
+        elif y=='выход':
+            print('Спасибо. Пока.')
             exit()
     else:
         text.repeat()
